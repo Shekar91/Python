58b4ffd7457b01c1544f2065a7bde31f7c77b471
Chandrashekar M <35068274+Shekar91@users.noreply.github.com>
2019-08-04 22:56:43 +0530

    Add files via upload

---
+++ KmstoMiConvertor.py
@@ -0,0 +1,7 @@
+# Kilometer to Mile Convertion
+
+print("Enter the Kilometers you ran today?");
+kilometers= input();
+mi= float(kilometers)*0.621371;
+mi=round(mi,3);
+print(f"The {kilometers} Kms you ran is equal to {mi} miles");
